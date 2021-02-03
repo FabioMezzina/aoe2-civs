362b8cb0ad875d9545346073be394c80e4eaf15d
FabioMezzina <fabio.mezzina.09@gmail.com>
2021-02-03 16:02:14 +0100

    add fakedb + logo images

---
+++ src/assets/database/civdb.js
@@ -0,0 +1,28 @@
+const aoedb = [
+  {
+    name: "aztecs",
+    imgUrl: "/aztecs.png"
+  },
+  {
+    name: "berbers",
+    imgUrl: "/berbers.png"
+  },
+  {
+    name: "britons",
+    imgUrl: "/britons.png"
+  },
+  {
+    name: "bulgarians",
+    imgUrl: "/bulgarians.png"
+  },
+  {
+    name: "burmese",
+    imgUrl: "/burmese.png"
+  },
+  {
+    name: "byzantines",
+    imgUrl: "/byzantines.png"
+  }
+];
+
+export default aoedb;

747ee3e9acecfdab90272d3bab3c303ea710e9d7
FabioMezzina <fabio.mezzina.09@gmail.com>
2021-02-03 16:13:15 +0100

    card + img works

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div id="app">
-
+    <Card
+      v-for="(civ, i) in civdb"
+      v-bind:key="i"
+      :name="civ.name"
+      :imgUrl="civ.imgUrl"
+    />
   </div>
 </template>
 
 <script>
+import civdb from "./assets/database/civdb.js";
+import Card from "./components/Card.vue";
 
 export default {
   name: "App",
   components: {
-
+    Card
+  },
+  data() {
+    return {
+      civdb: civdb,
+    }
   }
 };
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <img :src="'assets/images' + imgUrl" alt="img non caricata">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ["name", "imgUrl"]
+}
+</script>
+
+<style>
+
+</style>

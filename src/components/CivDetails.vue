e2c9b7da81249d28ba85cd557e03feef103585c4
FabioMezzina <fabio.mezzina.09@gmail.com>
2021-02-05 14:39:00 +0100

    pass civ name to civDetails, watch for name change and get right civilization from db

--- src/components/CivDetails.vue
+++ src/components/CivDetails.vue
@@ -1,14 +1,30 @@
 <!-- The detailed informations for a civilization -->
 <template>
   <div>
-    La civiltà selezionata è: {{ name }}
+    La civiltà selezionata è: {{ civ.name }}
+    <img :src="`assets/images${civ.imgUrl}`" alt="img non caricata">
   </div>
 </template>
 
 <script>
+import civdb from "../assets/database/civdb.js";
+
 export default {
   name: "CivDetails",
-  props: ["name"]
+  props: ["name"],
+  data() {
+    return {
+      civdb: civdb,
+      civ: {}
+    }
+  },
+  watch: {
+    name: function(value) {
+      this.civ = civdb.find(civ => {
+        return civ.name === value;
+      })
+    }
+  }
 }
 </script>
 
